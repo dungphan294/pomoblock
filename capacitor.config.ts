fc42a882ed79acf0e3eb0587744f9436a2ce38c7
v-phan-voortman <v.phan@voortman.net>
2026-05-20 14:52:21 +0200

    Initialize App
    Ionic, Capacitor, and Angular TypeScript framework.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pomoblock',
+  webDir: 'www'
+};
+
+export default config;
